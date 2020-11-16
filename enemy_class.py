b393f9ef14cf35ed485607862cf05b3a77b63120
Rohan Suri <rohan@vitatech.co>
2020-11-16 16:11:15 -0700

    Tested Functionality

--- enemy_class.py
+++ enemy_class.py
@@ -4,8 +4,9 @@ import random
 vec = pygame.math.Vector2 
 #enemy bit states 0 is fast pursuit, 1 is slow pursuit, 2 is targeted 3 is random
 class Enemy:
-    def __init__(self, app, pos, name, enemy_bit_state):
+    def __init__(self, app, pos, name, bit_state):
         self.app = app
+        self.enemy_bit_state = bit_state
         self.name = name
         self.current_grid_pos = pos
         self.current_pix_pos = self.getPixPos();
@@ -16,18 +17,19 @@ class Enemy:
         self.player_target = None
         self.radii = int(self.app.cell_width//2.3)
         self.speed = self.getSpeed()
-        self.enemy_movement = self.getEnemyMovement()
-        self.enemy_bit_state = enemy_bit_state
-    def getPixPos():
-        x = (self.grid_pos.x*self.app.cell_width)+TOP_BOTTOM_MARGIN//2+self.app.cell_width//2
-        y = (self.grid_pos.y*self.app.cell_height)+TOP_BOTTOM_MARGIN//2 + self.app.cell_height//2
+        #self.enemy_movement = self.getEnemyMovement()
+        
+    def getPixPos(self):
+        x = (self.current_grid_pos.x*self.app.cell_width)+TOP_BOTTOM_MARGIN//2+self.app.cell_width//2
+        y = (self.current_grid_pos.y*self.app.cell_height)+TOP_BOTTOM_MARGIN//2 + self.app.cell_height//2
         return vec(x,y)
                    
-    def getSpeed():
-        if self.enemy_bit_state !=1:
-            speed = 2
+    def getSpeed(self):
+        if self.enemy_bit_state != 1:
+            self.speed = 2
         else:
-            speed = 1
+            self.speed = 1
+        return self.speed;
     '''
     def getEnemyMovement(self):
         if self.enemy_bit_state==0:
@@ -37,21 +39,21 @@ class Enemy:
         else:
             return "targeted"
     '''
-    def getTarget():
+    def getTarget(self):
         if self.enemy_bit_state==0 or self.enemy_bit_state ==1:
-            return self.app.player.current_grid_pos
+            return self.app.player.grid_pos
         else:
             if self.app.player.current_grid_pos[0] > COLS//2 and self.app.player.current_grid_pos[1] > ROWS//2:
                 vec(1,1)
             if self.app.player.current_grid_pos[0] > COLS//2 and self.app.player.current_grid_pos[1] < ROWS//2:
                 return vec(1, ROWS-2)
-            if self.app.player.grid_pos[0] < COLS//2 and self.app.player.grid_pos[1] > ROWS//2:
+            if self.app.player.current_grid_pos[0] < COLS//2 and self.app.player.current_grid_pos[1] > ROWS//2:
                 return vec(COLS-2, 1)
             else:
                 return vec(COLS-2, ROWS-2)
 
        
-    def canMove():
+    def canMove(self):
         x = int(self.pix_pos.x+TOP_BOTTOM_MARGIN//2) % self.app.cell_width
         y = int(self.pix_pos.y+TOP_BOTTOM_MARGIN//2) % self.app.cell_height
         if  x == 0:
@@ -61,7 +63,7 @@ class Enemy:
             if self.direction == vec(0, 1) or self.direction == vec(0, -1) or self.direction == vec(0, 0):
                 return True
         return False
-    def enemyMove():
+    def enemyMove(self):
         if(self.enemy_bit_state == 0 or self.enemy_bit_state==1 or self.enemy_bit_state==2):
             self.direction = self.findNextPos(self.player_target)
         if(self.enemy_bit_state==3):
@@ -71,10 +73,10 @@ class Enemy:
 
     def setCurrentPixPos(self):
         if self.player_target != self.current_grid_pos:
-            self.pix_pos += self.direction * self.speed
-            return true
+            self.current_pix_pos += self.direction * self.speed
+            return True
     def findNextPos(self,player_target):
-        vec_pos = self.BreadthFirstSearchEnemyTarget([int(self.grid_pos.x), int(self.grid_pos.y)], [int(player_target[0]), int(player_target[1])])[1]
+        vec_pos = self.BreadthFirstSearchEnemyTarget([int(self.current_grid_pos.x), int(self.current_grid_pos.y)], [int(player_target[0]), int(player_target[1])])[1]
         delta_x = vec_pos[0]-self.current_grid_pos[0] 
         delta_y = vec_pos[1]-self.current_grid_pos[1]
         return vec(delta_x,delta_y)
@@ -110,47 +112,47 @@ class Enemy:
                     shortest.insert(0, step["Current"])
         return shortest
     def pixPos_To_GridPos_X(self):
-        self.grid_pos[0] = (self.pix_pos[0]-TOP_BOTTOM_MARGIN + self.app.cell_width//2)//self.app.cell_width+1
+        self.current_grid_pos[0] = (self.current_pix_pos[0]-TOP_BOTTOM_MARGIN + self.app.cell_width//2)//self.app.cell_width+1
 
     def pixPos_To_GridPos_Y(self):
-        self.grid_pos[1] = (self.pix_pos[1]-TOP_BOTTOM_MARGIN + self.app.cell_height//2)//self.app.cell_height+1
+        self.current_grid_pos[1] = (self.current_pix_pos[1]-TOP_BOTTOM_MARGIN + self.app.cell_height//2)//self.app.cell_height+1
 
     def updateEnemyState(self):
         self.player_target = self.getTarget()
-        if self.setCurrentPixPos() == true:
-            if self.canMove == true:
+        if self.setCurrentPixPos() == True:
+            if self.canMove == True:
                 self.enemyMove()
-        pixPos_To_GridPos_X()
-        pixPos_To_GridPos_Y()
+        self.pixPos_To_GridPos_X()
+        self.pixPos_To_GridPos_Y()
 class BlueEnemy(Enemy):
     def draw(self):
-        pygame.draw.circle(self.app.screen, (0, 255, 255), (int(self.pix_pos.x), int(self.pix_pos.y)), self.radii)
+        pygame.draw.circle(self.app.screen, (0, 255, 255), (int(self.current_pix_pos.x), int(self.current_pix_pos.y)), self.radii)
 
 class OrangeEnemy(Enemy):
     def draw(self):
-        pygame.draw.circle(self.app.screen, (255, 184, 82), (int(self.pix_pos.x), int(self.pix_pos.y)), self.radii)
+        pygame.draw.circle(self.app.screen, (255, 184, 82), (int(self.current_pix_pos.x), int(self.current_pix_pos.y)), self.radii)
 
 class RedEmemy(Enemy):
     def draw(self):
-        pygame.draw.circle(self.app.screen, (255, 0, 0), (int(self.pix_pos.x), int(self.pix_pos.y)), self.radii)
+        pygame.draw.circle(self.app.screen, (255, 0, 0), (int(self.current_pix_pos.x), int(self.current_pix_pos.y)), self.radii)
 
 
 class PinkEmemy(Enemy):
     def draw(self):
-        pygame.draw.circle(self.app.screen, (255, 184, 255), (int(self.pix_pos.x), int(self.pix_pos.y)), self.radii)
+        pygame.draw.circle(self.app.screen, (255, 184, 255), (int(self.current_pix_pos.x), int(self.current_pix_pos.y)), self.radii)
 
 
 class EnemyFactory():
-    def CreateEnemy(self,type):
+    def CreateEnemy(self, type, app, pos, name, bit_state):
         if(type == "Blue"):
-            return BlueEnemy()
+            return BlueEnemy(app, pos, name, bit_state)
         elif(type=="Orange"):
-            return OrangeEnemy()
+            return OrangeEnemy(app, pos, name, bit_state)
         elif(type=="Red"):
-            return RedEmemy()
+            return RedEmemy(app, pos, name, bit_state)
         elif(type=="Pink"):
-            return PinkEmemy()
-        return None
+            return PinkEmemy(app, pos, name, bit_state)
+        #return None
 
 if __name__ == '__main__':
     #instance = Enemy()
